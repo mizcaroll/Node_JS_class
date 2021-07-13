48c93741472c4eb22fecd3c2cf9eba0fc42e58f3
mizcaroll <carolomukule@gmail.com>
2021-07-13 12:07:48 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//create own event by importing an event emmittter from the events module built into node
+const { EventEmitter } = require('events');
+
+//create a custom events module by instanciating a class
+//console.log(global.luckyNum);
+console.log(process.plarform)
