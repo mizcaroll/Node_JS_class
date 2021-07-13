d5d5e2990ce81f0b415d460f23c3545dda8947b7
mizcaroll <carolomukule@gmail.com>
2021-07-13 12:25:42 +0300

    events and callbacks

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 //create own event by importing an event emmittter from the events module built into node
 const { EventEmitter } = require('events');
 
-//create a custom events module by instanciating a class
-//console.log(global.luckyNum);
-console.log(process.plarform)
+//create a custom event emmitter by instanciating a class
+const eventEmitter = new EventEmitter();
+
+//next we'll register a callback that fires on the lunch event
+eventEmitter.on('lunch', () => {
+    console.log('yum');
+})
+
+//call eventEmitter.emit() with the event name to trigger the callback function to run
+eventEmitter.emit('lunch');
